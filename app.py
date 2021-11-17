ddc3e3f2a27bede88d71adfa9b9391fe5d3d8d35
Tabea Heckenthaler <tabea.heckenthaler@weizmann.ac.il>
2021-11-17 09:52:32 +0200

    Added the details page.

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, render_template
 from os import listdir
 import json
 import pandas as pd
-import markdown
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -11,36 +10,43 @@ STUDENTS_DIRECTORY = open("config.txt", 'r').read()
 
 
 @app.route("/")
-def main():
-    # df = create_df(STUDENTS_DIRECTORY)
-    # index = find_indices()
-    # mark_string = single_student(df.loc[index])
+def main() -> str:
     return render_template("index.html")
 
-# @app.route("/")
-# def main():
-#     df = create_df(STUDENTS_DIRECTORY)
-#     index = find_indices()
-#     mark_string = single_student(df.loc[index])
-#     return markdown.markdown(mark_string)
+
+@app.route("/search_result", methods=['POST'])
+def search_result() -> str:
+    indices = find_indices(str(request.form.get('search_word', '')))
+    return render_template('index.html', students=[df.loc[index]['name'] for index in indices])
 
 
-def find_indices():
-    return 17
-
-
-def single_student(serie):
+@app.route('/details/<name>')
+def details(name) -> str:
     """
-    :return: markdown string for a series of a student.
+    :param name: name of student
+    :return: html string displaying the details of the student
     """
-    return '[' + serie['name'] + "](" + serie['exercism_profile'] + ")"
+    student = df[df['name'] == name].iloc[0].to_dict()
+    student = {k: v for k, v in student.items() if v.__class__ == str}
+    return render_template("details.html", student=student)
 
 
-def create_df(dir):
+def find_indices(search_word: str) -> list:
+    """
+    :param search_word: string, which to look for in entire DataFrame
+    """
+    s = df.apply(lambda row: row.astype(str).str.contains(search_word).any(), axis=1)
+    return s[s].index
+
+
+def create_df(dir) -> pd.DataFrame:
+    """
+    :param dir: path where the student jsons can be found
+    :return: pandas Dataframe with all the students information
+    """
     students_jsons = listdir(dir)
     dicts = [json.load(open(dir + students_json)) for students_json in students_jsons]
     return pd.DataFrame(dicts)
 
 
-if __name__ == '__main__':
-    print(main())
+df = create_df(STUDENTS_DIRECTORY)
